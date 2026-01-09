e4789c6b00798d63f5ab4b4eac26f4f0a66da4ea
frederickcorredor <144303183+frederickcorredor@users.noreply.github.com>
2026-01-09 13:26:13 -0500

    Implement sound activation and overlay interaction
    Added functionality to activate sound on overlay click and improved event handling.

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-
-
-
 const video = document.getElementById("video1");
+const overlay = document.getElementById("soundOverlay");
 const boton = document.getElementById("ctaBtn");
 
-
-video.addEventListener("click", () => {
-    video.muted = false;
-});
-
-// 1 minuto 23 segundos = 82 segundos
 const tiempoMostrar = 82;
 
+// activar sonido al tocar overlay o video
+const activarSonido = () => {
+    video.muted = false;
+    overlay.style.display = "none";
+};
+
+// interacción válida
+overlay.addEventListener("click", activarSonido);
+video.addEventListener("click", activarSonido);
+
+// CTA por tiempo
 video.addEventListener("timeupdate", () => {
     if (video.currentTime >= tiempoMostrar) {
         boton.classList.add("activo");
     }
 });
-
-
